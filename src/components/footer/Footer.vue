6132c6efd2f17ff2ad761f960aa87cf7148a7215
jb <jasontbrown99@gmail.com>
2019-12-02 16:04:15 -0800

    initial commit

---
+++ src/components/footer/Footer.vue
@@ -0,0 +1,23 @@
+<template>
+  <footer>
+    <div class="container">
+      <div>
+        <img :src="logoIcon">
+        <div>Call Us</div>
+        <div>123-456-1818</div>
+      </div>
+    </div>
+  </footer>
+</template>
+
+<script>
+  import Logo from '../../assets/images/logo.png';
+
+  export default {
+    data() {
+      return {
+        logoIcon: Logo
+      }
+    }
+  }
+</script>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,14 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
+import { MdButton, MdDrawer } from 'vue-material/dist/components'
+import 'vue-material/dist/vue-material.min.css'
+import VueCarousel from 'vue-carousel';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+Vue.use(MdButton);
+Vue.use(MdDrawer);
+Vue.use(VueCarousel);
 
 new Vue({
   render: h => h(App),
